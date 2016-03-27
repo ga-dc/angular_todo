7b4039e3b39d134436ce807719787bbea7646a0c
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-03-27 15:12:34 -0400

    can delete from list

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
 var data =
 [{"name":"grocery store","due":"03/27/2016"},
-{"name":"pick up car from shop","due":"03/28/2016"}];
+{"name":"go to auto shop","due":"03/28/2016"},
+{"name":"do Angular hw","due":"03/31/2016"}];
