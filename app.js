e3b4c6318b690ff6be80c7c23bdd31e810a41de2
Melissa Miller <lis.d.miller@gmail.com>
2016-03-26 22:16:05 -0400

    Added module and controller

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function(){
+  angular
+  .module('task', [])
+  .controller('task_controller', TaskController );
+
+  function TaskController() {
+    var vm = this;
+  }
+
+})();
