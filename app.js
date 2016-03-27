0f6dd314a8e2a4563b9790f5b1847abee5126171
Cam <camlatimer@gmail.com>
2016-03-27 15:17:30 -0400

    added id's, ng-attributes, etc to view, added function to add tasks in controller

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 "use strict";
+
+(function(){
+  angular.module('toDoApp', []).controller('list_controller', ListController);
+  var tasks = [{name: 'dinner'}];
+  function ListController(){
+    var vm = this;
+    vm.tasks = tasks;
+    vm.taskAdder = function(){
+      var taskBox = document.getElementById('task-field').value;
+      if(taskBox !== '') {
+        vm.tasks.push({
+          name: taskBox
+        });
+      }
+      document.getElementById('task-field').value = '';
+    };
+    vm.deleteTask = function(){
+      
+    };
+  }
+})();
