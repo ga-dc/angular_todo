96113c48cc0fc0315efb105048f4482577ccb313
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-27 19:03:56 -0400

    added created and delete

--- app.js
+++ app.js
@@ -6,8 +6,19 @@
     .controller("todo_controller", TodoController);
   function TodoController(){
     var viewmod = this;
-    viewmod.data = "I've got shit to do!";
+    viewmod.data = data;
+    viewmod.sort_data_by = function(item){
+      viewmod.sort_on = item;
+      viewmod.is_descending = !(viewmod.is_descending);
+    };
+    viewmod.destroy = function(item){
+      var index = viewmod.data.indexOf(item);
+      viewmod.data.splice(index, 1);
+    };
+    viewmod.new_item = {};
+    viewmod.create = function(){
+      viewmod.data.push(angular.copy(viewmod.new_item));
+      viewmod.new_item = {};
+    };
   }
-  
-
 })();
