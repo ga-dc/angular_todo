a7e87d2ce39e140e9016db4ee2ecc0c0a6ae33a3
Christine Movius <christine.movius@gmail.com>
2016-03-27 13:56:55 -0400

    User can delete todo items.

--- app.js
+++ app.js
@@ -8,13 +8,18 @@
   function TodoController() {
     var vm = this;
     vm.data = data;
+
+    vm.destroy = function(todo){
+      var index = vm.data.indexOf(todo);
+      vm.data.splice(index, 1);
+    };
+
+    vm.new_task = {};
+    vm.create = function(){
+      vm.data.push(angular.copy(vm.new_task));
+      vm.new_task = {};
+    };
+
   }
-
-
-
-
-
-
-
 
 })();
