ac3903192573c6160cc2d834aa154a4f567fcc07
Bipin Neupane <icomp2@hotmail.com>
2016-03-29 12:33:12 -0400

    commit

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 "use strict";
+
+//having funtion in the beginning like this is an iife its an angular convention to use it.
+
+
+(function(){
+angular
+.module('todolist', []);
+.controller('app_controller', ControllerFunction);
+
+  function ControllerFunction(){
+    var vm = this;
+    vm.list = [ "eat food",
+                "take shit",
+                "code",
+                "sleep"
+              ];
+
+})();
