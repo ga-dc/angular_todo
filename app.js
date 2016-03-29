e2704edf14b45184291762e73dfc7a3b5f1992ad
Charles <charlesthwang@gmail.com>
2016-03-29 13:20:06 -0400

    set up table in index.html & added module and controller

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ var todoList = [
   {task: "Buy groceries"},
   {task: "Call mom"};
   {task: "Clean the house"};
+];
 
-  (function(){
-    angular
-      
-  })();
+(function(){
+  angular
+    .module("todo", [])
+    .controller("todo_list", ToDoList);
+
+  function ToDoList(){
+    
+  }
+
+})();
