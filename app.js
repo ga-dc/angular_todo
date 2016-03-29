2abbb78acebd0ed4ade7f0f261eaa530227973e7
Bipin Neupane <icomp2@hotmail.com>
2016-03-29 18:36:33 -0400

    listed items, add button, css

--- app.js
+++ app.js
@@ -2,27 +2,48 @@
 
 //having funtion in the beginning like this is an iife its an angular convention to use it.
 
+//todoApp is the name of the module
+// []representes an empty array where dependencies can be injected.
+// TodoListController is the name of the controller
+angular.module("todoApp", [])
+.controller("TodoListController", function(){
+  var todoList =  [
+    {text:"Declare that Bipin is invincible", done:true},
+    {text:"Make sun revolve around me", done:true},
+    {text:"Learn basic programming", done:false}
+  ];
+  this.todos = todoList;
 
-(function(){
-angular
-.module("todolist", [])
-.controller("app_controller", ControllerFunction);
+todoList.addTodo = function(){
+  todoList.todos.push({text:todoList.todoText, done:false});
+  todoList.todoText = "";
 
-  function ControllerFunction(){
-    var vm = this;
-    vm.list = [
-      "eat food",
-              "drink water",
-              "code",
-              "sleep"
-              ];
-
-vm.new_item = {};
-vm.create = function() {
-  vm.list.push(angular.copy(vm.new_item));
-  vm.new_item = {};
-}
- };
+};
+});
 
 
-})();
+
+
+
+
+//
+// );
+//
+//   function ControllerFunction(){
+//     var vm = this;
+//     vm.list = [
+//       "eat food",
+//               "drink water",
+//               "code",
+//               "sleep"
+//               ];
+//
+// vm.new_item = {};
+// vm.create = function() {
+//   vm.list.push(angular.copy(vm.new_item));
+//   vm.new_item = {};
+// }
+//  };
+//
+//
+// })();
