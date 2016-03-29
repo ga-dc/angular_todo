bd945b08efd53e204f7fd72615066d80edcd8e82
Craig Eisen <Ceisen91@gmail.com>
2016-03-29 14:33:00 -0400

    It's up and running

--- app.js
+++ app.js
@@ -24,14 +24,36 @@
       minutes = (minutes < 10 ? '0' : '') + minutes;
       return hours + ":" + minutes;
     }
+    vm.modify = function(task){
+      var index = vm.data.indexOf(task);
+      vm.data[index].edit = true;
+    };
+    vm.update = function(task){
+      var index = vm.data.indexOf(task);
+      vm.data[index].edit = false;
+    };
     vm.destroy = function(task){
       var index = vm.data.indexOf(task);
       vm.data.splice(index, 1);
     }
-    vm.new_task = {};
+    vm.new_task = {edit: false};
     vm.create = function(){
       vm.data.push(angular.copy(vm.new_task));
       vm.new_task = {};
     }
   }
 })();
+
+
+var app = angular.module('addApp', []);
+
+app.controller('modifyCtrl', ['$scope', function($scope){
+    $scope.vm.data= [
+        {'name':'rohit', 'dob':'15-august-1985', 'emailId':'rohit@rohit.com', 'phone':'9999999999', 'address':'Delhi Rohini', 'id':'0' },
+        {'name':'aman', 'dob':'26-july-1975', 'emailId':'haryanat@hr.com', 'phone':'9874563210', 'address':'Haryana Sonepat', 'id':'1' },
+        {'name':'devraj', 'dob':'27-march-1980', 'emailId':'punjab@punjab.com', 'phone':'7410258963', 'address':'Punjab AmritSar', 'id':'2' }
+    ];
+
+
+
+}]);
--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-var data = [{"priority": 1,"title":"Do GA homework", "description":"Angular Assignments","ettc":120},
-{"priority": 2,"title":"Review Class Material", "description":"Look at class material from 3/25 and 3/28","ettc": 60},
-{"priority": 3,"title":"Exercise", "description":"LOL","ettc": 60}]
+var data = [{"priority": 1,"title":"Do GA homework", "description":"Angular Assignments","ettc":120, "edit": false},
+{"priority": 2,"title":"Review Class Material", "description":"Look at class material from 3/25 and 3/28","ettc": 60, "edit": false},
+{"priority": 3,"title":"Exercise", "description":"LOL","ettc": 60, "edit": false}]
