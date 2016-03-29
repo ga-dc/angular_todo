d283cb18a0f3ef3f227b8e09e81d14c7a430bb77
Craig Eisen <Ceisen91@gmail.com>
2016-03-29 19:46:04 -0400

    refined code

--- app.js
+++ app.js
@@ -43,17 +43,3 @@
     }
   }
 })();
-
-
-var app = angular.module('addApp', []);
-
-app.controller('modifyCtrl', ['$scope', function($scope){
-    $scope.vm.data= [
-        {'name':'rohit', 'dob':'15-august-1985', 'emailId':'rohit@rohit.com', 'phone':'9999999999', 'address':'Delhi Rohini', 'id':'0' },
-        {'name':'aman', 'dob':'26-july-1975', 'emailId':'haryanat@hr.com', 'phone':'9874563210', 'address':'Haryana Sonepat', 'id':'1' },
-        {'name':'devraj', 'dob':'27-march-1980', 'emailId':'punjab@punjab.com', 'phone':'7410258963', 'address':'Punjab AmritSar', 'id':'2' }
-    ];
-
-
-
-}]);
