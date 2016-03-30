b56809c37ca0395ead90311d7b4504db62ef528e
Toni Langley <chibia2@gmail.com>
2016-03-30 14:21:40 -0400

    todo complete

--- app.js
+++ app.js
@@ -2,12 +2,24 @@
 
 (function (){
   angular
-    .module("todo", [])
-    .controller("todo_controller", TodoController);
+  .module("todo", [])
+  .controller("todo_controller", TodoController);
 
-    function TodoController(){
-      var viewModel = this;
-      
-    }
+  function TodoController(){
+    var vm = this;
+    vm.task = data;
 
-})()
+    //creating things
+    vm.new_todo = {};
+    vm.create = function(){
+      vm.task.push(angular.copy(vm.new_todo));
+      vm.new_todo = {};
+    };
+
+    //deleting things
+    vm.delete = function(task_index){
+      vm.task.splice(task_index, 1);
+    };
+  } //ends todo controller
+
+})();
