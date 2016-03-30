da7d0ad837c53ad9fbb6f46c28a55de466a56d19
Woo Yun <hh747hh@gmail.com>
2016-03-29 22:10:47 -0400

    added show list, create function

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 "use strict";
 
 (function(){
-angular
-.module("todo" ,[] )
-.controller("todoController",ControllerFunction);
+  angular
+  .module("todo" ,[] )
+  .controller("todosController",ControllerFunction);
 
   function ControllerFunction(){
     var vm = this;
+    vm.todos = [
+      "Wake up",
+      "Go shopping",
+      "Eat food"
+    ];
+
+    vm.create = function(){
+      vm.todos.unshift(vm.content);
+      vm.content = "";
+    }
     
 
 
 
 
-}
+
+
+  }
+
 
 })();
