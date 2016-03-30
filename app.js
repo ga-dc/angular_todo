f667bf8c8775281e0e151c75be86e5959d922be5
Hilary Masland <hilarymasland@gmail.com>
2016-03-30 14:35:09 -0400

    ugh help gone wrong

--- app.js
+++ app.js
@@ -6,32 +6,25 @@
 
   function ControllerFunction(){
     var vm = this;
-    vm.tasks = [
-      {text: "eat lunch"}
-    ];
+    vm.tasks = [""];
 
 
 
 
   vm.create = function(){
     console.log("hello");
-    vm.tasks.push({text: vm.new_task});
+    vm.tasks.push(vm.new_task);
     vm.new_task = "";
   };
 
 
-  vm.edit = function(task){
-    console.log(task);
-    task.isediting = true;
-        // var tasks = vm.task[index];
-        // vm.new_task = task;
+  vm.edit = function(index){
+    console.log(index);
+    console.log(vm.edit);
+    console.log(vm.tasks);
+    vm.isediting = false;
+    vm.tasks[index] = vm.edit;
       };
 
-  vm.update = function(){
-    console.log(task);
-    task.update = true;
-  }
-        };
-
-
+}
 })();
