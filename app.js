a1886cf77a767665cd03468bfeae3b4b1ca023ab
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-30 00:30:44 -0400

    started over again and added visible array of tasks

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 "use strict";
-(function(){
-  angular
-  .module("ToDo", [])
+
+var app = angular.module('TaskBastard', []);
+
+app.controller('TBController', function(){
+this.tasks = tasks;
+console.log(this.tasks);
+
+});
 
 
-})();
+
+
+// (function(){
+//   angular
+//   .module("TaskBastard", [])
+//   .controller("AppController", AppController
+//   );
+//
+//   function AppController(){
+//     var vm = this;
+//     vm.taskList = data;
+//     console.log(vm.taskList);
+//
+//
+//     vm.newTask = function(){
+//       vm.taskList.push({task: vm.taskText})
+//
+//
+//     };
+//   }
+// })();
+// // these close out iffe
---
+++ data.js
@@ -0,0 +1,18 @@
+var tasks = [
+  {
+    taskContent: "Do Homework",
+    dueDate: "Tomorrow"
+  },
+  {
+    taskContent: "Learn JavaScript",
+    dueDate: "Yesterday"
+  },
+  {
+    taskContent: "Hug Bipin",
+    dueDate: "Today"
+  },
+  {
+    taskContent: "Build the SuperApp",
+    dueDate: "Next Week"
+  }
+];
