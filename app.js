41b924f0e7ccb2ff474cdbf405b12c3635cbb900
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-30 14:30:14 -0400

    edit box added ?

--- app.js
+++ app.js
@@ -7,9 +7,29 @@
   .controller("ToDo_App", ToDoFunction);
  function ToDoFunction(){
   var vm = this;
-  vm.name = "To Do !";
+  vm.todos = [
+    "Go to sleep",
+    "Go to sleep",
+    "Go to sleep",
+  ];
+ 
 
- }
+   vm.todos = function(){
 
+   };
+   vm.new = function(){
 
+   };
+   vm.edit = function($scope){
+   };
+
+   vm.create = function(){
+   };
+   vm.update = function(){
+
+     vm.content=todo;
+   };
+   vm.delete = function(){
+
+   };
 })();
