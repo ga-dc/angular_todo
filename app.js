6fa458dfb79529edf905c8b6181748ad1b1f44e2
Melissa Miller <lis.d.miller@gmail.com>
2016-03-30 19:04:24 -0400

    Adds editing ability

--- app.js
+++ app.js
@@ -17,15 +17,6 @@
       vm.new_task = '';
     }; // end of create
 
-    vm.edit = function(index) {
-      var todo = vm.data[index];
-      vm.content = todo;
-    }; // end of edit
-
-    vm.update = function(index) {
-      vm.data[index] = vm.content;
-    }; // end of update
-
     vm.destroy = function(index) {
       vm.data.splice(index, 1);
     }; // end of destroy
