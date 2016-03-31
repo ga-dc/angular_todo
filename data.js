c3e867f50aab9b03347700aa5a05247ce2ddf559
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-31 07:27:38 -0400

    HW submission

---
+++ data.js
@@ -0,0 +1,6 @@
+var data = [
+{"item":"Laundry","note":"Pick up laundry"},
+{"item":"Exercise","note":"run 5 miles"},
+{"item":"Groceries","note":"buy milk, bread..."},
+{"item":"Code","note":"Research ReactJS"},
+{"item":"Dog Grooming","note":"Drop dog off and at PETCO"}];
