e6780258a7fb86cf23d14189b6c56ab176e3afb5
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-30 23:45:45 -0400

    got the form to line up and accurate number of cells

--- app.js
+++ app.js
@@ -8,5 +8,13 @@
   function ToDoListController(){
     var vm = this;
     vm.data = data;
-  }
+    vm.destroy = function(list_index){
+      vm.data.splice(list_index, 1);
+    }
+    vm.new_item = {};
+    vm.create = function(){
+      vm.data.push(angular.copy(vm.new_item));
+      vm.new_item = {};
+    }
+  };
 }());
