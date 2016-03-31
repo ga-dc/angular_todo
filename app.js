7cb5044878b974f1962ae65663bbcf50dc55d4a0
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-30 21:10:47 -0400

    Error needs to be fixed

--- app.js
+++ app.js
@@ -12,24 +12,28 @@
     "Go to sleep",
     "Go to sleep",
   ];
- 
+
 
    vm.todos = function(){
 
    };
-   vm.new = function(){
 
-   };
-   vm.edit = function($scope){
+   vm.edit = function(index){
+     var todo = vm.todos[index];
+     vm.content =todo;
    };
 
    vm.create = function(){
+     vm.todos.unshift(vm.content);
+     vm.content = "";
    };
    vm.update = function(){
+     vm.todos[index] = vm.content;
 
      vm.content=todo;
    };
-   vm.delete = function(){
-
-   };
+   vm.delete = function(index){
+     vm.todos.splice( index, 1);
+   }
+ }
 })();
