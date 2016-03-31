50a3b6ff4a9f198373e9c54e858a5cc8fa673eb2
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-31 00:43:16 -0400

    very frustrated

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 'use strict';
 
-(function() {
+(function(){
   angular
-  .module("lists", [] )
-  .controller("toDoListController", ToDoListController);
+  .module("todo", [] )
+  .controller("todolistController", ControllerFunction);
 
-  function ToDoListController(){
+  function ControllerFunction(){
     var vm = this;
     vm.data = data;
-    vm.destroy = function(list_index){
-      vm.data.splice(list_index, 1);
-    }
 
-    vm.new_list = {};
     vm.create = function(){
-      vm.data.push(angular.copy(vm.new_list));
-      vm.new_list = {};
+      vm.data.push(angular.copy(vm.new_todo));
+      vm.new_todo = {};
+    };
+
+    vm.edit = function(index){
+      var data = vm.data[index];
+      vm.content = data;
+    };
+
+    vm.update = function(index){
+      vm.data[index] = vm.content;
+    };
+
+    vm.destroy = function(todo_index){
+      vm.data.splice(todo_index, 1);
     }
   };
 }());
