0a94f0ea45dba39ff534027f60a34bc49db9c5d9
Matthew Lamar Kerns <matthewkerns@ymail.com>
2016-03-31 09:00:18 -0400

    Fixed fork error

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+(function () {
+  angular
+  .module("myTODO", [])
+  .controller("todoController", ControllerFunction);
+
+  function ControllerFunction() {
+    var vm = this;
+    vm.todos = data;
+
+  }
+})();
