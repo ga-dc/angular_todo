51e501254ad6da975a88fd7c21949b63a8bf47d4
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-30 21:33:52 -0400

    more dummy data added and a form in index app

--- data.js
+++ data.js
@@ -1,2 +1,2 @@
 var data =
-[{"item": "Learn to Code"}, {"item": "Go To The Gym"}, {"item": "Apply to Jobs"}, {"item": "Go to the Grocery"}]
+[{"item": "Learn to Code", "Comments": "Spend 2 hours on this tonight. Finish To Do List and Grumblr App"}, {"item": "Go To The Gym", "Comments": "Complete the HIIT workout and body-weight circuit #2"}, {"item": "Apply to Jobs", "Comments": "Ensure resume complete. Finish GA profile page."}, {"item": "Go to the Grocery", "Comments": "Get food to cook for this weekend."}]
