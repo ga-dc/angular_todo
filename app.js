a1e272adfd2b5ef115d2f9ea8dd062381ea41d59
Zaneh Williams <zanehmw@gmail.com>
2016-05-01 18:28:04 -0400

    i think im doing this right? added a link that says hello

--- app.js
+++ app.js
@@ -3,13 +3,18 @@
 (function() {
   angular
   .module('todo', [])
-  .controller("todosController", function(){
-    this.todos = [
-      "Walk the dog",
-      "Buy groceries",
-      "Drink coffee",
-      "Wake up like this"
-    ]
-  });
+  .controller("todosController", ControllerFunction);
 
 })()
+
+
+function ControllerFunction(){
+  var vm = this;
+  this.todos = [
+    "Walk the dog",
+    "Buy groceries",
+    "Drink coffee",
+    "Wake up like this"
+  ];
+
+}
