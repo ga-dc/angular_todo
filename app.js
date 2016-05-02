b7c061f1b0a2165ef7a79eacf5c983d760aafcb5
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-01 21:27:26 -0400

    initial commit, most of barebones complete

---
+++ app.js
@@ -0,0 +1,23 @@
+"use strict";
+
+(function(){
+  angular
+  .module("todo", [])
+  .controller("todo_controller", TodoController);
+
+  function TodoController(){
+    var vm = this;
+    vm.data = data;
+    vm.editing = false;
+    vm.addTodo = function(){
+      vm.data.push({"task":this.task, "priority":this.priority});
+    };
+    vm.edit = function(i){
+      vm.data[i] = {"task":this.task_edit, "priority":this.priority_edit}
+    };
+    vm.delete = function(i){
+      vm.data.splice(i, 1);
+    };
+  };
+
+})();
---
+++ data.js
@@ -0,0 +1,5 @@
+var data = [
+  {"task": "wash cats", "priority": "low"},
+  {"task": "high five two tall people", "priority": "super high"},
+  {"task": "go canoeing", "priority": "medium"}
+]
