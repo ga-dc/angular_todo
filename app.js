4bb8f762d539febcaa9ccb2d41bf7edec764005c
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-02 09:43:46 -0400

    clear text on submit

--- app.js
+++ app.js
@@ -8,9 +8,11 @@
   function TodoController(){
     var vm = this;
     vm.data = data;
-    vm.editing = false;  
+    vm.editing = false;
     vm.addTodo = function(){
       vm.data.push({"task":this.task, "priority":this.priority});
+      vm.task = '';
+      vm.priority = '';
     };
     vm.update = function(i){
       vm.data[i] = {"task":this.task_edit, "priority":this.priority_edit};
