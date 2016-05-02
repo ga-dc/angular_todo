1f5283ecd24c893f6d5c952938848ea87288a640
Louis <LOHankins@gmail.com>
2016-05-02 09:04:44 -0400

    First commit for todo app

---
+++ app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+(function(){
+  angular
+   .module("todo", [])
+   .controller("todo", ToDo);
+  
+   function ToDo(){
+     var vm = this;
+     vm.data = data;
+     // vm.sort_data_by = function(name){
+     //   vm.sort_on = name;
+     //   vm.is_descending = !(vm.is_descending);
+     // }
+     vm.total_value = function(){
+       var total = 0;
+       vm.data.forEach(function(product){
+         if(product.quantity){
+           total += (product.quantity * product.cost);
+         }
+       });
+       return total.toFixed(2);
+     };
+    vm.destroy = function(product_index){
+      vm.data.splice(product_index, 1);
+    }
+    vm.new_product = {};
+     vm.create = function(){
+       vm.data.push(angular.copy(vm.new_product));
+       vm.new_product = {};
+     }
+ }
+})();
