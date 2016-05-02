29abcfb45298cc0fb18c0302a1c1a5086c89cc99
Mary Griffus <mary.e.shultz@gmail.com>
2016-05-01 21:45:46 -0400

    fix updating, edit form still always visible

--- app.js
+++ app.js
@@ -8,12 +8,11 @@
   function TodoController(){
     var vm = this;
     vm.data = data;
-    vm.editing = false;
     vm.addTodo = function(){
       vm.data.push({"task":this.task, "priority":this.priority});
     };
-    vm.edit = function(i){
-      vm.data[i] = {"task":this.task_edit, "priority":this.priority_edit}
+    vm.update = function(i){
+      vm.data[i] = {"task":this.task_edit, "priority":this.priority_edit};
     };
     vm.delete = function(i){
       vm.data.splice(i, 1);
